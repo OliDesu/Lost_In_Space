6c3a0da4a7dccbc912ea012c3c8437d368d58224
William Nelson <swilliam.nelson@gmail.com>
2020-11-18 16:15:15 +0100

    Projet 2D se situe maintenant dans le projet 3D (Project)

---
+++ Projet/Assets/Scripts/TextScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScript : MonoBehaviour
+{
+
+    public Text text;
+
+    public static int score;
+    public static int lives;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = "Score :";
+        score = GameManager.score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Score : " + GameManager.score;
+    }
+}
