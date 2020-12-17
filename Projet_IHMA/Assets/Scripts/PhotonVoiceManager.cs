6d9736a7f77cca1558336a089396b8ce3aee1185
William Nelson <swilliam.nelson@gmail.com>
2020-12-17 20:18:06 +0100

    tentative vocal

--- Projet_IHMA/Assets/Scripts/PhotonVoiceManager.cs
+++ Projet_IHMA/Assets/Scripts/PhotonVoiceManager.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+using Photon.Pun;
+using Photon.Voice.Unity;
+using Photon.Voice.PUN;
 
 public class PhotonVoiceManager : MonoBehaviour
 {
+
+    bool talk = false;
+
+    public Recorder recorder;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonVoiceNetwork.Instance.Client.GlobalInterestGroup = 0;
+        this.recorder = GetComponent<Recorder>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Talk()
     {
-        
+        if (!talk)
+        {
+            recorder.TransmitEnabled = true;
+        } else {
+            recorder.TransmitEnabled = false;
+        }
     }
+
 }
